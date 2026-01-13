397a488543c2bb981a19b4699eea27d234688513
Gabriel Merencio Ortega <147098148+SysGabrielMO@users.noreply.github.com>
2026-01-13 12:37:33 +0100

    Delete funcionesmariadb.py

--- funcionesmariadb.py
+++
@@ -1,9 +0,0 @@
-import sys
-import MySQLdb
-try:
-	db = MySQLdb.connect("localhost","admingabriel","usuario","libreria" )
-except MySQLdb.Error as e:
-	print("No puedo conectar a la base de datos:",e)
-	sys.exit(1)
-print("Conexión correcta.")
-db.close()
